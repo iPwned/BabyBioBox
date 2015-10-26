7232ae3e123c2540be53cff3e0cf473f90352803
Christopher Betancourt <christopher.betancourt@gmail.com>
2015-10-26 14:33:48 -0400

    Completed base account creation code.

--- dancer/lib/BabyBioBox.pm
+++ dancer/lib/BabyBioBox.pm
@@ -1,7 +1,10 @@
 package BabyBioBox;
 use Dancer2;
-use DBI;
+use DBI qw(:sql_types);
 use Template;
+use Data::Entropy::Algorithms qw(rand_bits);
+use MIME::Base64;
+use Digest;
 
 our $VERSION = '0.1';
 
@@ -9,6 +12,7 @@ set 'session'=>'Simple';
 set 'database'=>'/tmp/test.db'; #change this for production
 set 'template'=>'template_toolkit';
 
+use constant BCRYPT_COST => 10; #selected for performance on the hosting box.
 
 my $glbl_message='';
 
@@ -72,13 +76,56 @@ any ['get','post']=>'/logout'=> sub{
 any ['get','post']=>'/create_account'=>sub{
 	if(request->method() eq 'POST')
 	{
+		unless(params->{'email'} && params->{'password'} && params->{'confirm_pass'})
+		{
+			set_message('All fields are required.  Please fill in all fields and try again');
+			return redirect '/create_account';
+		}
+		unless(params->{'password'} eq params->{'confirm_pass'})
+		{
+			set_message('Password and password confirmation must match.  Please try again.');
+			return redirect '/create_account';
+		}
 		my $dbh=db_open();
-		#do account creation things
+		#check for existance of account.
+		my $query='select count(*) from users where user_email=?';
+		my $sth=$dbh->prepare($query);
+		$sth->bind_param(1,params->{email},SQL_VARCHAR);
+		$sth->execute();
+		my $count=$sth->fetch();
+		$sth->finish();
+		if($$count[0])
+		{
+			set_message('Account exists.  Please contact your admin if you need your password reset');
+			$dbh->disconnect();
+			return redirect '/create_account';
+		}
+		#at this point it's probably reasonably safe to actually attempt the account creation.
+		#IMPORTANT: this uses the default entropy source.  Good enough for a small project but
+		#	not so much so for a larger one.
+		my $salt=rand_bits(8*16);
+		my $bcrypt=Digest->new('Bcrypt');
+		#scrubbing of inputs should really be done prior to this.
+		$bcrypt->cost(BCRYPT_COST);
+		$bcrypt->salt($salt);
+		$bcrypt->add(params->{password});
+		#now, get the base64 versions that will go into the db.
+		$salt=encode_base64($salt);
+		my $b64_pass=$bcrypt->b64digest;
+		$query='insert into users (user_email,salt,pw_hash) values (?,?,?)';
+		$sth=$dbh->prepare($query);
+		$sth->bind_param(1,params->{email},SQL_VARCHAR);
+		$sth->bind_param(2,$salt,SQL_CHAR);
+		$sth->bind_param(3,$b64_pass,SQL_CHAR);
+		$sth->execute();#should check to make sure this succeedes.
+		$sth->finish();
 		$dbh->disconnect();
-		session 'logged_in'=>1;#replace with real creation verification logic
+		set_message('Account created.  Your admin will have to authorize this account before you can log in');
 		return redirect '/';
 	}
-	template 'create';
+	template 'create',{
+		'message'=>get_message()
+	};
 };
 
 true;
